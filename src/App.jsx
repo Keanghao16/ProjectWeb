dcc17e0f5a1277aa0d0ea961b32e36f318c211b6
Keanghao16 <tiekeangaho778899@gmail.com>
2024-01-04 14:47:45 +0700

    place detail dynamic compeleted

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import DiscoverPage from "./pages/DiscoverPage";
-
+import PlaceDetailPage from "./pages/PlaceDetailPage"
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <HomePage />,
+      // element: <HomePage />,
+      element: <PlaceDetailPage />,
     },
     {
       path: "/discover",
