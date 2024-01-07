56e239eaf13bb4b102be21cef222e269ae915057
s02t <sokphanun@gmail.com>
2024-01-07 10:57:53 +0700

    OwO what's this

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 
@@ -6,158 +7,187 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 const AboutPage = () => {
+  const [isPopupActive, setIsPopupActive] = useState(false);
+
   useEffect(() => {
     AOS.init();
   }, []);
   return (
     <>
-      <Nav currentPage="AboutPage" headerElementId="header" />
-
-      <section id="about" className="about">
-        <div className="container" data-aos="fade-up">
-          <div className="section-title">
-            <h2>About Us</h2>
-          </div>
-
-          <div className="row content">
-            <div className="col-lg-6">
-              <p>
-                We're a team of aspiring individuals who have never actually
-                traveled in our lives but spend everyday sitting in front of a
-                computer browsing for places to put into this website.
-              </p>
-              <ul>
-                <li>
-                  <i className="ri-check-double-line"></i> I didn't sign up for
-                  this, where's the easy part{" "}
-                </li>
-                <li>
-                  <i className="ri-check-double-line"></i> I had a culture shock
-                  when I first entered the className
-                </li>
-                <li>
-                  <i className="ri-check-double-line"></i> I wanna kill myself
-                </li>
-              </ul>
+      <Nav
+        currentPage="AboutPage"
+        headerElementId="header"
+        isPopupActive={isPopupActive}
+        setPopupActive={setIsPopupActive}
+      />
+      <div className={` ${isPopupActive ? "blur-background" : ""}`}>
+        <section id="about" className="about">
+          <div className="container" data-aos="fade-up">
+            <div className="section-title">
+              <h2>About Tripplaner</h2>
             </div>
-            <div className="col-lg-6 pt-4 pt-lg-0">
-              <p>
-                We're really not that advanced in terms of programming, but
-                we're trying our best right now so please if you're reading this
-                teacher pls pls pls give us good grades T-T we are struggling so
-                hard rn whoever tf said univserity is fun they fucking lied cant
-                believe i fell for it i wanna go back to elementary.
-              </p>
-              <a href="#" className="btn-learn-more">
-                Learn More
-              </a>
+
+            <div className="row content">
+              <div className="col-lg-6">
+                <p>
+                  &nbsp; &nbsp;Trip Planner, the world's largest travel guidance
+                  platform, helps hundreds of millions of people each month
+                  become better travelers, from planning to taking a trip.
+                  Travelers in Cambodia should use the Trip Planner site to
+                  discover where to stay, what to do and where to eat based on
+                  guidance from those who have been there before. Travelers turn
+                  to Trip Planner to find deals on accommodations, reserve
+                  tables at delicious restaurants and discover great places
+                  nearby.
+                </p>
+              </div>
+              <div className="col-lg-6 pt-4 pt-lg-0">
+                <p>
+                  &nbsp; &nbsp;Travel is not just a journey from one place to
+                  another; it's an experience that unfolds a myriad of stories
+                  and memories. In the bustling world we live in, where time is
+                  a precious commodity, the role of a trip planner becomes
+                  invaluable. A trip planner is more than a guide; it's a
+                  navigator, a storyteller, and a curator of extraordinary
+                  adventures.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      <section id="team" className="team section-bg">
-        <div className="container" data-aos="fade-up">
-          <div className="section-title">
-            <h2>Team</h2>
-            <p>
-              As you can see we only have 3 people in this team, which is if my
-              math is correct, not a lot. We need help with API
-            </p>
-          </div>
+        </section>
+        <section id="team" className="team section-bg">
+          <div className="container" data-aos="fade-up">
+            <div className="section-title">
+              <h2>Our Team</h2>
+              <p>Hello everyone, I'd like to introduce our team members</p>
+            </div>
 
-          <div className="row">
-            <div className="col-lg-6" data-aos="zoom-in" data-aos-delay="100">
-              <div className="member d-flex align-items-start">
-                <div className="pic">
-                  <img src="img/teammember.jpg" className="img-fluid" alt="" />
+            <div className="row">
+              <div className="col-lg-6" data-aos="zoom-in" data-aos-delay="100">
+                <div className="member d-flex align-items-start">
+                  <div className="pic">
+                    <img
+                      src="public/image/nun.jpg"
+                      className="img-fluid"
+                      alt=""
+                    />
+                  </div>
+                  <div className="member-info">
+                    <h4>Thou Sokphanun</h4>
+                    <span>Team Member</span>
+                    <p>
+                      "I'm Sokphanun, thrilled to be part of the Trip Planner
+                      team."
+                    </p>
+                    <p>
+                      <i>Nun's favorite travel destination is Kampot.</i>
+                    </p>
+                    <div className="social">
+                      <a href="">
+                        <i className="bx bxl-twitter"></i>
+                      </a>
+                      <a href="">
+                        <i className="bx bxl-facebook"></i>
+                      </a>
+                      <a href="">
+                        <i className="bx bxl-instagram"></i>
+                      </a>
+                      <a href="">
+                        {" "}
+                        <i className="bx bxl-linkedin"></i>{" "}
+                      </a>
+                    </div>
+                  </div>
                 </div>
-                <div className="member-info">
-                  <h4>Thou Sokphanun</h4>
-                  <span>Chief Executive Officer</span>
-                  <p></p>
-                  <div className="social">
-                    <a href="">
-                      <i className="bx bxl-twitter"></i>
-                    </a>
-                    <a href="">
-                      <i className="bx bxl-facebook"></i>
-                    </a>
-                    <a href="">
-                      <i className="bx bxl-instagram"></i>
-                    </a>
-                    <a href="">
-                      <i className="bx bxl-linkedin"></i>
-                    </a>
+              </div>
+
+              <div
+                className="col-lg-6 mt-4 mt-lg-0"
+                data-aos="zoom-in"
+                data-aos-delay="200"
+              >
+                <div className="member d-flex align-items-start">
+                  <div className="pic">
+                    <img
+                      src="public/image/hao.jpg"
+                      className="img-fluid"
+                      alt=""
+                    />
+                  </div>
+                  <div className="member-info">
+                    <h4>Tie Keanghao</h4>
+                    <span>Team Member</span>
+                    <p>
+                      I'm Keanghao, absolutely thrilled to join the Trip Planner
+                      team.
+                    </p>
+                    <p>
+                      <i>Hao's favorite travel destination is Koh Kong.</i>
+                    </p>
+                    <div className="social">
+                      <a href="">
+                        <i className="bx bxl-twitter"></i>
+                      </a>
+                      <a href="">
+                        <i className="bx bxl-facebook"></i>
+                      </a>
+                      <a href="">
+                        <i className="bx bxl-instagram"></i>
+                      </a>
+                      <a href="">
+                        {" "}
+                        <i className="bx bxl-linkedin"></i>{" "}
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                className="col-lg-6 mt-4"
+                data-aos="zoom-in"
+                data-aos-delay="300"
+              >
+                <div className="member d-flex align-items-start">
+                  <div className="pic">
+                    <img
+                      src="public/image/lily.jpg"
+                      className="img-fluid"
+                      alt=""
+                    />
+                  </div>
+                  <div className="member-info">
+                    <h4>Thon Ratha</h4>
+                    <span>Team Member</span>
+                    <p>
+                      I'm Ratha, and I couldn't be more thrilled to join the
+                      Trip Planner team.
+                    </p>
+                    <p>
+                      <i>Ratha's favorite travel destination is Mondulkiri.</i>
+                    </p>
+                    <div className="social">
+                      <a href="">
+                        <i className="bx bxl-twitter"></i>
+                      </a>
+                      <a href="">
+                        <i className="bx bxl-facebook"></i>
+                      </a>
+                      <a href="">
+                        <i className="bx bxl-instagram"></i>
+                      </a>
+                      <a href="">
+                        {" "}
+                        <i className="bx bxl-linkedin"></i>{" "}
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-
-            <div
-              className="col-lg-6 mt-4 mt-lg-0"
-              data-aos="zoom-in"
-              data-aos-delay="200"
-            >
-              <div className="member d-flex align-items-start">
-                <div className="pic">
-                  <img src="img/teammember.jpg" className="img-fluid" alt="" />
-                </div>
-                <div className="member-info">
-                  <h4>Tie Keanghao</h4>
-                  <span>Product Manager</span>
-                  <p>jg yy tha ot dg</p>
-                  <div className="social">
-                    <a href="">
-                      <i className="bx bxl-twitter"></i>
-                    </a>
-                    <a href="">
-                      <i className="bx bxl-facebook"></i>
-                    </a>
-                    <a href="">
-                      <i className="bx bxl-instagram"></i>
-                    </a>
-                    <a href="">
-                      <i className="bx bxl-linkedin"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div
-              className="col-lg-6 mt-4"
-              data-aos="zoom-in"
-              data-aos-delay="300"
-            >
-              <div className="member d-flex align-items-start">
-                <div className="pic">
-                  <img src="img/teammember.jpg" className="img-fluid" alt="" />
-                </div>
-                <div className="member-info">
-                  <h4>Thon Ratha</h4>
-                  <span>CTO</span>
-                  <p>jg yy thaaaaaaaaaaaaaaaaaaaaa</p>
-                  <div className="social">
-                    <a href="">
-                      <i className="bx bxl-twitter"></i>
-                    </a>
-                    <a href="">
-                      <i className="bx bxl-facebook"></i>
-                    </a>
-                    <a href="">
-                      <i className="bx bxl-instagram"></i>
-                    </a>
-                    <a href="">
-                      <i className="bx bxl-linkedin"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
-        </div>
-      </section>
-      {/* popup */}
+        </section>
+      </div>
 
       <Footer />
     </>
--- src/pages/DiscoverPage.jsx
+++ src/pages/DiscoverPage.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
 import Nav from "../components/Nav";
 
 import Province from "../data/Province.json";
@@ -7,53 +8,63 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 const DiscoverPage = () => {
+  const [isPopupActive, setIsPopupActive] = useState(false);
+
   useEffect(() => {
     AOS.init();
   }, []);
 
   return (
     <>
-      <Nav currentPage="DiscoverPage" headerElementId="header" />
-      <main id="main">
-        <section id="discover" className="disc">
-          <div className="container discovertitle" data-aos="fade-up">
-            <div className="section-title">
-              <h2>Popular Places</h2>
-              <p>
-                <i>Get inspiration for your next trip</i>
-              </p>
-            </div>
-          </div>
-        </section>
-      </main>
+      <Nav
+        currentPage="DiscoverPage"
+        headerElementId="header"
+        isPopupActive={isPopupActive}
+        setPopupActive={setIsPopupActive}
+      />
 
-      <div className="container dis">
-        <div className="row gy-3 my-3">
-          {Province &&
-            Province.map((province) => (
-              <div
-                className="col-sm-6 col-md-4 col-lg-3 province-card"
-                key={province.id}
-              >
-                <a href="#" className="card-link">
-                  <div className="card" data-aos="zoom-out">
-                    <img
-                      src={province.img}
-                      className="card-img-top card-img-fixed"
-                      alt={`Province ${province.province}`}
-                    />
-                    <div className="card-body">
-                      <h5 className="card-title province">
-                        {province.province}
-                      </h5>
-                      <p className="card-text description">
-                        {province.description}
-                      </p>
-                    </div>
-                  </div>
-                </a>
+      <div className={` ${isPopupActive ? "blur-background" : ""}`}>
+        <main id="main">
+          <section id="about" className="about">
+            <div className="container discovertitle" data-aos="fade-up">
+              <div className="section-title">
+                <h2>Popular Places</h2>
+                <p>
+                  <i>Get inspiration for your next trip</i>
+                </p>
               </div>
-            ))}
+            </div>
+          </section>
+        </main>
+
+        <div className="container dis">
+          <div className="row gy-3 my-3">
+            {Province &&
+              Province.map((province) => (
+                <div
+                  className="col-sm-6 col-md-4 col-lg-3 province-card"
+                  key={province.id}
+                >
+                  <a href="#" className="card-link">
+                    <div className="card" data-aos="zoom-out">
+                      <img
+                        src={province.img}
+                        className="card-img-top card-img-fixed"
+                        alt={`Province ${province.province}`}
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title province">
+                          {province.province}
+                        </h5>
+                        <p className="card-text description">
+                          {province.description}
+                        </p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </>
